3f4abcb4fafc394ea31bbf55579f49f8a8344b2f
Anass Lamouri <anasslamouri@icloud.com>
2025-08-27 15:53:19 +0100

    Starter code #js

---
+++ script.js
@@ -0,0 +1,18 @@
+// Import helper to map currency to country flags
+import { currencyToFlagCode } from './currency-to-flag-code.js'
+
+// Select elements from the DOM
+
+// Swap source and target currencies
+
+// Update exchange rate upon input
+
+// Perform conversion when button is clicked
+
+// Update exchange rate displayed
+
+// Change country flags upon select
+
+// Initialize select menus and flags
+
+// Format currency
