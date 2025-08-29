77eb075b9dc179003137c7ec86af2fb810d2e712
Anass Lamouri <anasslamouri@icloud.com>
2025-08-29 15:15:21 +0100

    Initialize select menus and flags #js

--- script.js
+++ script.js
@@ -3,6 +3,8 @@ import { currencyToFlagCode } from './currency-to-flag-code.js'
 
 // Select elements from the DOM
 const currencyConverterSelect = document.querySelectorAll('.currency-converter__select select')
+const imageSourceCurrency = document.getElementById('imageSourceCurrency')
+const imageTargetCurrency = document.getElementById('imageTargetCurrency')
 // Swap source and target currencies
 
 // Update exchange rate upon input
@@ -14,5 +16,33 @@ const currencyConverterSelect = document.querySelectorAll('.currency-converter__
 // Change country flags upon select
 
 // Initialize select menus and flags
+currencyConverterSelect.forEach(selectElement => {
+  for (const [currency, flagCode] of Object.entries(currencyToFlagCode)) {
+    const newOptionElement = document.createElement('option')
+    newOptionElement.value = currency
+    newOptionElement.textContent = currency
+    selectElement.appendChild(newOptionElement)
+  }
+  selectElement.addEventListener('change', () => {
+    changeFlag(selectElement)
+  })
+  if (selectElement.id === 'selectSourceCurrency') {
+    selectElement.value = 'MAD'
+  }
+  if (selectElement.id === 'selectTargetCurrency') {
+    selectElement.value = 'MYR'
+  }
+})
 
+function changeFlag(selectElement) {
+  const selectValue = selectElement.value
+  const selectId = selectElement.id
+  const flagCode = currencyToFlagCode[selectValue]
+
+  if (selectId === 'selectSourceCurrency') {
+    imageSourceCurrency.src = `https://flagcdn.com/w640/${flagCode}.png`
+  } else {
+    imageTargetCurrency.src = `https://flagcdn.com/w640/${flagCode}.png`
+  }
+}
 // Format currency
